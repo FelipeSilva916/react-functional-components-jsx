af198166022f431fb937b883ca2b5ac3565ec6e3
Felipe Silva <felipesilva916@gmail.com>
2022-06-09 19:07:45 -0700

    setup 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import Showcase from "./Showcase";
-import "./App.css";
 import BaseStats from "./BaseStats";
+import "./App.css";
 
 function App() {
   const baseStats = {
     hp: 45,
     attack: 49,
-    defense: 49,
+    defense: 48,
     spAttack: 65,
-    spDef: 65,
-    speed: 45
+    spDef: 66,
+    speed: 47
   };
 
   const handleClick = () => {
     alert(
-      `Special Stats\n\tSpecial Attack: ${baseStats.spAttack}\n\tSpecial Defense: ${baseStats.spDef}`
+      `Special Stats:\n\tSpecial Attack: ${baseStats.spAttack}\n\tSpecial Defense: ${baseStats.spDef}`
     );
   };
+
   return (
     <div className="main-wrapper background">
       <Showcase />
-      <BaseStats stats={baseStats} clicker={handleClick} />
+      <BaseStats clicker={handleClick} stats={baseStats} />
     </div>
   );
 }
