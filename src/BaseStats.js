f88945550656f53af1a9d4203eb5e0745f0c4e3c
Felipe Silva <felipesilva916@gmail.com>
2022-06-09 19:00:32 -0700

    special button

--- src/BaseStats.js
+++ src/BaseStats.js
@@ -1,27 +1,30 @@
 import React from "react";
 import "./BaseStats.css";
 
-const BaseStats = ({ stats }) => {
+const BaseStats = ({ clicker, stats: { hp, attack, defense, speed } }) => {
   return (
     <div className="base-stats">
       <h1>BaseStats</h1>
+      <button className="sp-stats" onClick={clicker}>
+        Check Special Stats
+      </button>
       <table>
         <tbody>
           <tr>
             <td>Hit Points</td>
-            <td>{stats.hp}</td>
+            <td>{hp}</td>
           </tr>
           <tr>
             <td>Attack</td>
-            <td>{stats.attack}</td>
+            <td>{attack}</td>
           </tr>
           <tr>
             <td>Defense</td>
-            <td>{stats.defense}</td>
+            <td>{defense}</td>
           </tr>
           <tr>
             <td>Speed</td>
-            <td>{stats.speed}</td>
+            <td>{speed}</td>
           </tr>
         </tbody>
       </table>
