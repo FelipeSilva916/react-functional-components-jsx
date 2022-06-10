aa8d6d66f3612fef8c8c06535960be4a0b5b7b93
Felipe Silva <felipesilva916@gmail.com>
2022-06-09 19:21:50 -0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,12 +3,12 @@ export const moves = [
   { id: 2, type: "normal", level: 1, move: "Tackle" },
   { id: 3, type: "water", level: 3, move: "Water Gun" },
   { id: 4, type: "normal", level: 6, move: "Bite" },
-  { id: 5, type: "water", level: 9, move: "leech seed" },
-  { id: 6, type: "water", level: 12, move: "razor leaf" },
-  { id: 7, type: "poison", level: 15, move: "poison powder" },
-  { id: 8, type: "water", level: 15, move: "sleep powder" },
-  { id: 9, type: "water", level: 18, move: "seed bomb" },
+  { id: 5, type: "normal", level: 9, move: "Skull Bash" },
+  { id: 6, type: "water", level: 12, move: "Hydro Pump" },
+  { id: 7, type: "poison", level: 15, move: "Poison Powder" },
+  { id: 8, type: "normal", level: 15, move: "Take Down" },
+  { id: 9, type: "water", level: 18, move: "Hydro Cannon" },
   { id: 10, type: "normal", level: 21, move: "take down" },
-  { id: 11, type: "normal", level: 24, move: "Hydro Pump" },
-  { id: 12, type: "water", level: 27, move: "Hydro Cannon" }
+  { id: 11, type: "ice", level: 24, move: "Ice Beam" },
+  { id: 12, type: "water", level: 27, move: "Flash Cannon" }
 ];
