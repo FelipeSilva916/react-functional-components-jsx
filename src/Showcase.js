3ce98fceb8fa4f9525e3589dd45f27ed82ea3945
Felipe Silva <felipesilva916@gmail.com>
2022-06-09 17:33:28 -0700

    set up 2

--- src/Showcase.js
+++ src/Showcase.js
@@ -1,3 +1,6 @@
+import blastoiseImg from "./images/avatar-blastoise.png";
+import "./Showcase.css";
+
 function Showcase() {
   const favPokemon = "Blastoise";
   const pokeCharacteristics = {
@@ -6,8 +9,15 @@ function Showcase() {
   };
 
   return (
-    <div>
+    <div className="showcase-wrapper">
       <h1>{favPokemon}'s Component</h1>
+      <img src={blastoiseImg} alt="fave pokemon Blastoise" />
+      <h2>
+        {favPokemon}'s type is{" "}
+        <span className="poke-type">{pokeCharacteristics.type}</span> and one of
+        their moves is{" "}
+        <span className="poke-move">{pokeCharacteristics.move}</span>.
+      </h2>
     </div>
   );
 }
