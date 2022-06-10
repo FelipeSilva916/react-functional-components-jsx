b47e6a3b61a84a67b18a2c59fd518c4901fd5f69
Felipe Silva <felipesilva916@gmail.com>
2022-06-09 17:10:24 -0700

    set up

---
+++ src/Showcase.js
@@ -0,0 +1,15 @@
+function Showcase() {
+  const favPokemon = "Blastoise";
+  const pokeCharacteristics = {
+    type: "Water",
+    move: "Surf"
+  };
+
+  return (
+    <div>
+      <h1>{favPokemon}'s Component</h1>
+    </div>
+  );
+}
+
+export default Showcase;
