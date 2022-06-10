27314a8d7937a532a311d936598149a66cd5b7f2
Felipe Silva <felipesilva916@gmail.com>
2022-06-09 19:18:17 -0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export const moves = [
-  { id: 1, type: "normal", level: 1, move: "growl" },
-  { id: 2, type: "normal", level: 1, move: "tackle" },
-  { id: 3, type: "grass", level: 3, move: "vine whip" },
-  { id: 4, type: "normal", level: 6, move: "growth" },
-  { id: 5, type: "grass", level: 9, move: "leech seed" },
-  { id: 6, type: "grass", level: 12, move: "razor leaf" },
+  { id: 1, type: "normal", level: 1, move: "Growl" },
+  { id: 2, type: "normal", level: 1, move: "Tackle" },
+  { id: 3, type: "water", level: 3, move: "Water Gun" },
+  { id: 4, type: "normal", level: 6, move: "Bite" },
+  { id: 5, type: "water", level: 9, move: "leech seed" },
+  { id: 6, type: "water", level: 12, move: "razor leaf" },
   { id: 7, type: "poison", level: 15, move: "poison powder" },
-  { id: 8, type: "grass", level: 15, move: "sleep powder" },
-  { id: 9, type: "grass", level: 18, move: "seed bomb" },
+  { id: 8, type: "water", level: 15, move: "sleep powder" },
+  { id: 9, type: "water", level: 18, move: "seed bomb" },
   { id: 10, type: "normal", level: 21, move: "take down" },
-  { id: 11, type: "normal", level: 24, move: "sweet scent" },
-  { id: 12, type: "grass", level: 27, move: "synthesis" }
+  { id: 11, type: "normal", level: 24, move: "Hydro Pump" },
+  { id: 12, type: "water", level: 27, move: "Hydro Cannon" }
 ];
